5e85f14750ebc4734365d95f3ba3fa7f85f9729b
Emmanuel Atuahene <emmanuelatuahene16@gmail.com>
2024-01-23 13:28:19 -0500

    mongo db set up

---
+++ route_structure.md
@@ -0,0 +1,8 @@
+##Route Structure
+
+-GET/POST/PUT/DELETE
+--/api/v1/bootcamps
+--/api/v1/courses
+--/api/v1/reviews
+--/api/v1/auth
+--/api/v1/users
--- server.js
+++ server.js
@@ -1,10 +1,27 @@
-const express =require("express")
-const dotenv = require("dotenv")
+const express = require('express');
+const dotenv = require('dotenv');
+const morgan = require('morgan');
+const connectDB=require('./config/db')
 
-dotenv.config({path:'./config/config.env'})
+dotenv.config({ path: './config/config.env' });
 
-const app=express()
+//connect to DB
+connectDB();
 
-const PORT =process.env.PORT || 5000;
+//Route files
+const bootcamps = require('./routes/bootcamps');
 
-app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV}`))
+
+const app = express();
+
+// Dev logging middleware
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
+
+//Mount Routers
+app.use('/api/v1/bootcamps', bootcamps);
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV}`));
